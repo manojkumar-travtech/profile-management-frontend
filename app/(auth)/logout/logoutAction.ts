bd4325265d8af411fad7c56e23025eb4ee3f674c
manojkumar-travtech <manojkumar@travtech.com>
2025-11-05 12:19:31 +0530

    "Second commit"

--- app/(auth)/logout/logoutAction.ts
+++
@@ -1,9 +0,0 @@
-// app/actions/logoutAction.ts
-"use server";
-
-import { cookies } from "next/headers";
-
-export async function logoutAction(): Promise<{ success: boolean }> {
-  (await cookies()).delete("access_token");
-  return { success: true };
-}
--- app/(eventManagement)/events/_components/CreateEventSteps/StepsComponents/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as EventDetails } from "./EventDetails";
-export { default as EventHandlers } from "./EventHandlers";
-export { default as Personalizing } from "./Personalizing";
-export { default as TravelRestrictions } from "./TravelRestrictions";
-export { default as EventPreviewCard } from "./EventPreviewCard";
